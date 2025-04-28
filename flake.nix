4c6b5ce88c13705af7d1b5bddcdcb7e201e30af7
glassesneo <glassesneo@protonmail.com>
2025-04-28 23:51:00 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    zls-overlay.url = "github:zigtools/zls/0.14.0";
+    zon2nix = {
+      url = "github:jcollie/zon2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    zig-overlay,
+    zls-overlay,
+    zon2nix,
+    ...
+  } @ inputs: let
+    allSystems = [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+    forAllSystems = fn: nixpkgs.lib.genAttrs allSystems (system: fn system nixpkgs.legacyPackages.${system});
+  in {
+    devShell = forAllSystems (
+      system: pkgs: let
+        zig = zig-overlay.packages.${system}."0.14.0";
+        zls = zls-overlay.packages.${system}.zls.overrideAttrs (old: {
+          nativeBuildInputs = [zig];
+        });
+        zon2nix = inputs.zon2nix.packages.${system}.zon2nix;
+      in
+        pkgs.mkShell {
+          packages = [
+            zig
+            zls
+            zon2nix
+          ];
+        }
+    );
+
+    packages = forAllSystems (
+      system: pkgs: {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "zig_flake";
+          version = "0.1.0";
+          src = ../.;
+          # deps = callPackage ./deps.nix {};
+          nativeBuildInputs = [
+            pkgs.zig_0_14.hook
+          ];
+        };
+      }
+    );
+  };
+}
