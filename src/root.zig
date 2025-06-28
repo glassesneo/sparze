c71b450f06914e26a76430944d2f7c7ea714c364
glassesneo <glassesneo@protonmail.com>
2025-06-28 21:28:13 +0900

    ci(test): setup actions for the test

--- src/root.zig
+++ src/root.zig
@@ -1,2 +1,8 @@
 pub const Entity = @import("core/entity.zig").Entity;
 pub const World = @import("core/world.zig").World;
+
+comptime {
+    std.testing.refAllDecls(@This());
+}
+
+const std = @import("std");
