379feb33b42ea00707b9bd23f93d1c3bd2ae18aa
neo <glassesneo@protonmail.com>
2025-08-26 00:03:44 +0900

    refactor: organize exports

--- src/root.zig
+++ src/root.zig
@@ -1,12 +1,12 @@
 const entity_module = @import("core/entity.zig");
-const EntityRegistry = entity_module.EntityRegistry;
-const Entity = entity_module.Entity;
-const getIndex = entity_module.getIndex;
-const getVersion = entity_module.getVersion;
+pub const EntityRegistry = entity_module.EntityRegistry;
+pub const Entity = entity_module.Entity;
+pub const getIndex = entity_module.getIndex;
+pub const getVersion = entity_module.getVersion;
 
 const sparse_set_module = @import("core/sparse_set.zig");
-const SparseSet = sparse_set_module.SparseSet;
-const AbstractSparseSet = sparse_set_module.AbstractSparseSet;
+pub const SparseSet = sparse_set_module.SparseSet;
+pub const AbstractSparseSet = sparse_set_module.AbstractSparseSet;
 
 test {
     std.testing.refAllDecls(@This());
